840c32657e6c75d656b933a7f9584d2095c2d0d1
Jakub Szymański <jakubszymanski@hotmail.com>
2023-04-03 22:55:01 +0200

    quiz view

--- src/app/views/adminViews/quiz/quiz.module.ts
+++ src/app/views/adminViews/quiz/quiz.module.ts
@@ -2,6 +2,9 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {QuizComponent} from "./quiz.component";
 import {RouterModule} from "@angular/router";
+import {CardModule} from "primeng/card";
+import {ButtonModule} from "primeng/button";
+import {RippleModule} from "primeng/ripple";
 
 
 @NgModule({
@@ -16,6 +19,9 @@ import {RouterModule} from "@angular/router";
         component: QuizComponent
       }
     ]),
+    CardModule,
+    ButtonModule,
+    RippleModule,
   ]
 })
 export class QuizModule { }
